1e8f52526d997344624e7babd366d9cb7d5f155c
Pázmányi Sándor <pazmanyi.sandor2004@gmail.com>
2024-05-23 18:54:56 +0200

    Create ChessApplication.java

---
+++ src/main/java/chess/control/ChessApplication.java
@@ -0,0 +1,23 @@
+package chess.control;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ChessApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/ui.fxml"));
+        stage.setTitle("Chess Game");
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+}
