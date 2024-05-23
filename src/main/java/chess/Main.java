62654c5d45c3d8c44bf5642c82ff34380a5746ed
Pázmányi Sándor <pazmanyi.sandor2004@gmail.com>
2024-05-23 17:02:14 +0200

    Running BFS algorithm

--- src/main/java/chess/Main.java
+++ src/main/java/chess/Main.java
@@ -1,7 +1,14 @@
 package chess;
 
+import chess.model.ChessState;
+import puzzle.solver.BreadthFirstSearch;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ChessState initialState = new ChessState(2, 1, 2, 2);
+
+        BreadthFirstSearch solver = new BreadthFirstSearch();
+
+        solver.solveAndPrintSolution(initialState);
     }
 }
