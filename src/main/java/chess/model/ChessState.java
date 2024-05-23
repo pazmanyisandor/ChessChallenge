ff8a30103dc055cddd7f9e8450ec167971304d16
Pázmányi Sándor <pazmanyi.sandor2004@gmail.com>
2024-05-23 16:52:57 +0200

    Finishing implementation of ChessState

--- src/main/java/chess/model/ChessState.java
+++ src/main/java/chess/model/ChessState.java
@@ -76,4 +76,64 @@ public class ChessState implements TwoPhaseMoveState<String> {
         int dy = Math.abs(newY - knightY);
         return (dx == 2 && dy == 1) || (dx == 1 && dy == 2);
     }
+
+    @Override
+    public void makeMove(TwoPhaseMove<String> move) {
+        if (isLegalMove(move)) {
+            String piece = move.from();
+            String[] parts = move.to().split(" ");
+            int newX = Integer.parseInt(parts[1]);
+            int newY = Integer.parseInt(parts[2]);
+            if (piece.equals("King")) {
+                kingX = newX;
+                kingY = newY;
+            } else if (piece.equals("Knight")) {
+                knightX = newX;
+                knightY = newY;
+            }
+        }
+    }
+
+    @Override
+    public Set<TwoPhaseMove<String>> getLegalMoves() {
+        Set<TwoPhaseMove<String>> legalMoves = new HashSet<>();
+        if (isLegalToMoveFrom("King")) {
+            int[][] kingMoves = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+            for (int[] move : kingMoves) {
+                int newX = kingX + move[0];
+                int newY = kingY + move[1];
+                if (newX >= 0 && newX < 8 && newY >= 0 && newY < 8) {
+                    legalMoves.add(new TwoPhaseMove<>("King", "King " + newX + " " + newY));
+                }
+            }
+        } else if (isLegalToMoveFrom("Knight")) {
+            int[][] knightMoves = {{-2, -1}, {-1, -2}, {1, -2}, {2, -1}, {2, 1}, {1, 2}, {-1, 2}, {-2, 1}};
+            for (int[] move : knightMoves) {
+                int newX = knightX + move[0];
+                int newY = knightY + move[1];
+                if (newX >= 0 && newX < 8 && newY >= 0 && newY < 8) {
+                    legalMoves.add(new TwoPhaseMove<>("Knight", "Knight " + newX + " " + newY));
+                }
+            }
+        }
+        return legalMoves;
+    }
+
+    @Override
+    public TwoPhaseMoveState<String> clone() {
+        return new ChessState(kingX, kingY, knightX, knightY);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessState that = (ChessState) o;
+        return kingX == that.kingX && kingY == that.kingY && knightX == that.knightX && knightY == that.knightY;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(kingX, kingY, knightX, knightY);
+    }
 }
